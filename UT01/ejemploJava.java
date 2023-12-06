2b3fd201a0df4e673019b70a78aa9b5a2a26af75
Gabriel González Carvajal <104269749+gabgcgt531g@users.noreply.github.com>
2023-12-06 19:17:40 +0100

    java coment

--- UT01/ejemploJava.java
+++ UT01/ejemploJava.java
@@ -1,3 +1,5 @@
+/*
+
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -8,7 +10,7 @@ public class miServlet extends HttpServlet {
 
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-
+		
 		int num1 = Integer.parseInt(req.getParameter("num1"));
 		int num2 = Integer.parseInt(req.getParameter("num2"));
 
@@ -21,6 +23,8 @@ public class miServlet extends HttpServlet {
 		out.println("    Prueba de servlet Java.");
 		out.println("    La suma de " + num1 + " + " + num2 + " es: " + (num1 + num2));
 		out.println("  </body>");
-		out.println("</html>");
+		out.println("</html>");		
 	}
 }
+
+*/
