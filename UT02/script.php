3635c068682dacab4916d21370dc6dace8ee8c1b
gabgcgt531g <gabgc.z930@gmail.com>
2023-12-16 19:41:03 +0100

    Empieza Tema 2

---
+++ UT02/script.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Tarea UT02</title>
+</head>
+
+<body>
+    <?php echo "¡Hola, soy un script de PHP!"; ?>
+</body>
+
+</html>
